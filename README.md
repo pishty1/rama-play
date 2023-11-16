237599be9b27f6ced44052e496b47ffe68f255a3
pishty1 <pishty1@gmail.com>
2023-11-16 20:45:16 +0000

    fix

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # rama play
-
-An empty Clojure project configured for developing with Rama.
-
-Note that Rama AOT compiles a specific Clojure version in its jar, so you can't specify a different version of Clojure in your dependencies.
--- src/rama_play/my_module.clj
+++ src/rama_play/my_module.clj
@@ -15,25 +15,25 @@
     (rm/declare-pstate s $$mypstate2 {Long String})
     (rm/<<sources s
                (rm/source> *mydepot :> {:keys [*key *value] :as *input})
-               (println "depot 1 adding " *key " and value " *value)
                (rm/local-transform> [(rp/keypath *key) (rp/termval *value)] $$mypstate)
                (rm/source> *mydepot2 :> {:keys [*key *value] :as *input})
-               (println "depot 2 adding " *key " and value " *value)
                (rm/local-transform> [(rp/keypath *key) (rp/termval *value)] $$mypstate2))))
 
 
 (comment
   (with-open [ipc (rtest/create-ipc)]
     (rtest/launch-module! ipc MyModule {:tasks 4 :threads 2})
-    (let [mymodule (get-module-name MyModule)
-          mydepot (foreign-depot ipc mymodule "*mydepot")
-          mydepot2 (foreign-depot ipc mymodule "*mydepot2")
-          mypstate (foreign-pstate ipc mymodule "$$mypstate")
-          mypstate2 (foreign-pstate ipc mymodule "$$mypstate2")
-          _ (foreign-append! mydepot (->MyRecord 1 "yeo"))
-          _ (foreign-append! mydepot2 (->MyRecord 1 "Hello world"))
-          myresult (foreign-select-one (keypath 1) mypstate)
-          myresult2 (foreign-select-one (keypath 1) mypstate2)]
+    (let [mymodule (rm/get-module-name MyModule)
+          mydepot (rm/foreign-depot ipc mymodule "*mydepot")
+          mydepot2 (rm/foreign-depot ipc mymodule "*mydepot2")
+          mypstate (rm/foreign-pstate ipc mymodule "$$mypstate")
+          mypstate2 (rm/foreign-pstate ipc mymodule "$$mypstate2")
+          _ (rm/foreign-append! mydepot (->MyRecord 1 "yeo"))
+          _ (rm/foreign-append! mydepot (->MyRecord 2 "yeo2"))
+          _ (rm/foreign-append! mydepot2 (->MyRecord 1 "Hello world"))
+          myresult (rm/foreign-select-one (rp/keypath 2) mypstate)
+          myresult2 (rm/foreign-select-one (rp/keypath 1) mypstate2)]
       (println "from depot 1 " myresult)
       (println "from depot 2 " myresult2)))
+
   )
